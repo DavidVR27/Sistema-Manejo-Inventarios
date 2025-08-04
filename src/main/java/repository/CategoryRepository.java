ebc8f827591f87ed684b237cd123dff0d5d20f01
David <orchestraaa69@gmail.com>
2025-08-03 19:25:20 -0500

    feat(repositories): created core repositories

---
+++ src/main/java/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import entity.Category;
+import org.hibernate.query.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
