cd795d9c5b4e237814b48f050030ad66cce5c4b4
David <orchestraaa69@gmail.com>
2025-08-04 00:20:36 -0500

    chore(category ent): fixed typo in data type

--- src/main/java/entity/Category.java
+++ src/main/java/entity/Category.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @NotBlank(message = "Nombre es requerido")
     @Column(unique = true)
