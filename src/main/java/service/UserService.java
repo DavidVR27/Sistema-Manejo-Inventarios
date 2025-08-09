c66411d3afe1acde1e1e04c1fb1251f230070dd0
David <orchestraaa69@gmail.com>
2025-08-09 12:33:51 -0500

    feat: added User Service

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,17 @@
+package service;
+
+import dto.LoginRequest;
+import dto.RegisterRequest;
+import dto.Response;
+import dto.UserDTO;
+import entity.User;
+
+public interface UserService {
+    Response registerUser(RegisterRequest registerRequest);
+    Response loginUser(LoginRequest loginRequest);
+    Response getAllUsers();
+    User getCurrentLoggedInUser();
+    Response updateUser(Long id, UserDTO userDTO);
+    Response deleteUser(Long id);
+    Response getUserTransactions();
+}
