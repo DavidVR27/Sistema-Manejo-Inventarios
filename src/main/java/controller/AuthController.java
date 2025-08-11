4b15d5bfdeeaff97a28e6591ee6218632ea23b0e
David <orchestraaa69@gmail.com>
2025-08-11 16:00:34 -0500

    feat: implemented login and register endpoints

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -1,4 +1,33 @@
 package controller;
 
+import dto.LoginRequest;
+import dto.RegisterRequest;
+import dto.Response;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> registerUser(@RequestBody @Valid RegisterRequest registerRequest){
+        return ResponseEntity.ok(userService.registerUser(registerRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> loginUser(@RequestBody @Valid LoginRequest loginRequest){
+        return ResponseEntity.ok(userService.loginUser(loginRequest));
+    }
+
+
 }
