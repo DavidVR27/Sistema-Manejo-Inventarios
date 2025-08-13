ad42da10c3c9a1c22cf53ce223dae58f3054f7a3
David <orchestraaa69@gmail.com>
2025-08-13 14:02:13 -0500

    feat: added component scan

--- src/main/java/com/davidvilela/SistemaManejoInventario/SistemaManejoInventarioApplication.java
+++ src/main/java/com/davidvilela/SistemaManejoInventario/SistemaManejoInventarioApplication.java
@@ -3,11 +3,13 @@ package com.davidvilela.SistemaManejoInventario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("entity")
 @EnableJpaRepositories("repository")
+@ComponentScan({"controller", "service", "security", "exceptions", "config"})
 public class SistemaManejoInventarioApplication {
 
 	public static void main(String[] args) {
