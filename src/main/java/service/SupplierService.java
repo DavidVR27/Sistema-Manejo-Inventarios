759ee0afdcce70ea0fa07bb3cb082115c7dc93e4
David <orchestraaa69@gmail.com>
2025-08-18 20:51:23 -0500

    fix: typo error

--- src/main/java/service/SupplierService.java
+++ src/main/java/service/SupplierService.java
@@ -4,8 +4,8 @@ import dto.Response;
 import dto.SupplierDTO;
 
 public interface SupplierService {
-    Response addSuppier(SupplierDTO supplierDTO);
-    Response updateSuppier(Long id, SupplierDTO supplierDTO);
+    Response addSupplier(SupplierDTO supplierDTO);
+    Response updateSupplier(Long id, SupplierDTO supplierDTO);
     Response getAllSuppliers();
     Response getSupplierById(Long id);
     Response deleteSupplier(Long id);
